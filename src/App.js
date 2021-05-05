54b70b9b28b11abb5aebb600f38a0333f5a164b9
Yaak0v <jacobsandry@gmail.com>
2021-05-04 22:54:24 -0500

    initialize repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex">
+      <div className="w-64 px-8 py-4 bg-gray-100">
+        <img className="h-9 w-9" src="img\E.svg" alt="Euclid Logo" />
+        <nav className='mt-8'>
+          <h2 className="text-xs font-semibold text-gray-600 uppercase tracking-wide">Issues</h2>
+          <a 
+          href="#" className='mt-2 flex justify-between items-center px-3 py-1 bg-gray-200 rounded-lg text-small font-medium text-gray-900'>
+            <span>All</span>
+            <span className="text-xs font-semibold text-gray-700">36</span>
+
+          </a>
+
+        </nav>
+      </div>
+      <div className="flex-1 min-w-0 bg-white">
+        <div className="border-b-2 border-gray-200"></div>
+        <div></div>
+      </div>
     </div>
   );
 }
